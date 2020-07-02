12b294ec4e29d0920c67685e0f4232f666386b0b
Raphael Puente <raphael.puente@gmail.com>
2020-07-01 23:41:08 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,51 @@
+/* Description: IIFE - Immediately Invokes Function Expression
+   File: Assignment2
+   Name: Raphael Puente 
+   Id: 301075627 */
+
+
+(function(){
+
+
+    
+    function Start()
+    {
+        console.log('My app started...')
+
+
+
+
+        
+        //Applying nav-link active to all the navbar
+
+
+        let title = document.title;
+        title = title.toLowerCase();
+
+        console.log(`The title of the page is ${title}`);
+        
+        let navAnchors = document.querySelectorAll("li a");
+
+        for (const anchor of navAnchors) {
+            
+            let anchorString = anchor.getAttribute("href");
+            anchorString = anchorString.substr(0,anchorString.length-5);
+
+            if ((title === "home") && (anchorString === "index") || (title === anchorString)) 
+            {
+                anchor.className = "nav-link active";   
+            }
+
+
+
+        }
+
+
+
+    }
+
+    window.addEventListener("load",Start);
+
+
+
+})();
